2a1624f7f4e4176135a21b4a514f3128af43b9fb
Dawson Fields <dfields16@build.tamu.edu>
2017-11-14 17:18:09 -0600

    Added constructor for GameManager

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,2 +1,20 @@
 #include "GameManager.h"
 
+GameManager::GameManager(Difficulty d):
+	difficulty(d)
+	{
+		switch(difficulty){
+			case Beginner:
+				//gamePtrn = BPTRN;
+			break;
+			case Intermediate:
+				//gamePtrn = IPTRN;
+			break;
+			case Advanced:
+				//gamePtrn = APTRN;
+			break;
+			case Expert:
+				//gamePtrn = EPTRN;
+			break;
+		}
+	}
